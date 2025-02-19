5b6d450edc1a2f8288ac9f705b00d0bd267a1fbe
Mo Baban <your-email@example.com>
2025-02-19 16:51:47 -0600

    Initial commit

---
+++ TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Mo Baban on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
